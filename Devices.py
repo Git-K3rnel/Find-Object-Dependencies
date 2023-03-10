f0851ff01fb07b8a8badcca85fdf425763476cce
_K3rnel <127470407+Git-K3rnel@users.noreply.github.com>
2023-03-10 09:54:47 +0330

    Create Devices.py

---
+++ Devices.py
@@ -0,0 +1,34 @@
+devices = [
+    {
+        #Fortigate1 Firewall
+        'device_type': 'fortinet',
+        'host': '',
+        'username': '',
+        'password': '',
+        'port': ''
+    },
+    {
+        #Fortigate2 Firewall
+        'device_type': 'fortinet',
+        'host': '',
+        'username': '',
+        'password': '',
+        'port': ''
+    },
+    {
+        #Fortigate3 Firewall
+        'device_type': '',
+        'host': '',
+        'username': '',
+        'password': '',
+        'port': ''
+    },
+    {
+        #SRX Firewall
+        'device_type': 'juniper_junos',
+        'host': '',
+        'username': '',
+        'password': '',
+        'port': ''
+    }
+]
